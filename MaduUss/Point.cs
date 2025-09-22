4e50b6e1ddc606a6901724b4e3c4d62e338a7015
opilane <opilane@HP-CZC2349HT3>
2025-09-22 10:06:08 +0300

    Add project files.

---
+++ MaduUss/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MaduUss
+{
+    class Point
+    { 
+        public int x;
+        public int y;
+        public char Sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(Sym);
+        }
+    } 
+}
---
+++ MaduUss/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaduUss
+{
+    class program
+    {
+        static void main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.Sym = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.Sym = '#';
+
+            p2.Draw();
+
+            Console.ReadLine();
+
+            /*int x1 = 1;
+            int y1 = 3;
+            char Sym1 = '*';
+
+            Draw( x1, y1, Sym1 );
+
+            int x2 = 4;
+            int y2 = 5;
+            char Sym2 = '#';
+
+            Draw( x2, y2, Sym2 );*/
+
+            Console.ReadLine();
+        }
+    }
+}
